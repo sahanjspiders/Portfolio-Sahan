4ef52cd6a5f042a40920f1182e06632b04f248b4
sahanjspiders <121511082+sahanjspiders@users.noreply.github.com>
2023-11-03 19:54:14 +0530

    Add files via upload

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Passionate and dedicated fresher front-end React.js developer,eager to shape exceptionaluser interfaces and elevate web experiences. Proficient in HTML,CSS and Javascript , with a focus on mastering React.js to build dynamic and engaging application. Committed to learning and impolementing best practices for responsive design,accessibility,and cross-browser. excited to contribute creative and technical skill to collaborative projects,seeking growth and impactful opportunities in world of web development",
+		keywords: ["Sahan", "Sahan G", "Sahan GAsti"],
+	},
+
+	{
+		page: "about",
+		description:
+			"Welcome to my About Page, where I'm excited to share my journey into the dynamic realm of web development through React.js. As a budding developer, I'm driven by a passion for creating immersive and intuitive digital experiences.",
+		keywords: ["Sahan", "Sahan G", "Sahan GAsti"],
+	},
+
+	{
+		page: "Resume",
+		description:
+			"iam Sahan",
+		keywords: ["Sahan", "Sahan G", "Sahan GAsti"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects ",
+		keywords: ["Sahan", "Sahan G", "Sahan GAsti"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project,",
+		keywords: ["Sahan", "Sahan G", "Sahan GAsti"],
+	},
+];
+
+export default SEO;
